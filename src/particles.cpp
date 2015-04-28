fbe4b00866e7c9ffa4abc273e8338b0904601193
zach lieberman <zach@eyebeam.org>
2015-04-28 11:32:18 -0400

    added particles

---
+++ src/particles.cpp
@@ -0,0 +1,9 @@
+//
+//  particles.cpp
+//  sfpcTest
+//
+//  Created by zach on 4/28/15.
+//
+//
+
+#include "particles.h"
---
+++ src/particles.h
@@ -0,0 +1,14 @@
+//
+//  particles.h
+//  sfpcTest
+//
+//  Created by zach on 4/28/15.
+//
+//
+
+#ifndef __sfpcTest__particles__
+#define __sfpcTest__particles__
+
+#include <iostream>
+
+#endif /* defined(__sfpcTest__particles__) */
